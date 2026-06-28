b51dc40fd070b8bc8fdd6a519b1544f1807963b8
emile-laurin <ejlaurin@wpi.edu>
2026-06-28 13:31:52 -0400

    Update Testing_LEDs_Arduino_1.ino

--- Testing_LEDs_Arduino_1/Testing_LEDs_Arduino_1.ino
+++ Testing_LEDs_Arduino_1/Testing_LEDs_Arduino_1.ino
@@ -1,13 +1,13 @@
 void setup() {
 	// Initialize serial communication - allows printing to the console for debugging.
 	Serial.begin(9600);
-	pinMode(2, OUTPUT); // Set the LED anode pin as an output
+	pinMode(LED1_Pin, OUTPUT); // Set the LED anode pin as an output
 }
 
 
 void loop() {
-  digitalWrite(2, HIGH); // Turn the LED on
+  digitalWrite(LED1_Pin, HIGH); // Turn the LED on
   delay(1000); // Wait for 1 second
-  digitalWrite(2, LOW); // Turn the LED off
+  digitalWrite(LED1_Pin, LOW); // Turn the LED off
   delay(1000); // Wait for 1 second before repeating
 }
