2fca573ac2c2368615b94a80651d79f8f3f8432e
emile-laurin <ejlaurin@wpi.edu>
2026-06-28 14:21:47 -0400

    Update Testing_LEDs_Arduino_1.ino

--- Testing_LEDs_Arduino_1/Testing_LEDs_Arduino_1.ino
+++ Testing_LEDs_Arduino_1/Testing_LEDs_Arduino_1.ino
@@ -1,13 +1,33 @@
+const int LED1_Pin=3; // LED Pin number on Arduino
+
+const int button1_Pin=2; // Button Pin number on Arduino
+
+int button1_state=0;
+
 void setup() {
 	// Initialize serial communication - allows printing to the console for debugging.
 	Serial.begin(9600);
 	pinMode(LED1_Pin, OUTPUT); // Set the LED anode pin as an output
+
+  pinMode(button1_Pin,INPUT); // Set the button pin as an inpit
 }
 
 
 void loop() {
-  digitalWrite(LED1_Pin, HIGH); // Turn the LED on
-  delay(1000); // Wait for 1 second
-  digitalWrite(LED1_Pin, LOW); // Turn the LED off
-  delay(1000); // Wait for 1 second before repeating
+
+  // Read Button State
+  button1_state = digitalRead(button1_Pin);
+  
+
+if (button1_state==HIGH)
+{
+  Serial.println("Button is Pressed"); // Print a message to the output
+   digitalWrite(LED1_Pin, HIGH); // Turn the LED on
+}
+else
+{
+  Serial.println("Button is no longer Pressed"); // Print a message to the output
+   digitalWrite(LED1_Pin, LOW); // Turn the LED off
+}
+  delay(100); // Small delay to avoid spamming the serial monitor
 }
